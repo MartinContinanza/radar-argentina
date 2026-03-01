659a42edd268e0c89e809c919d4e75b7f7272ef6
MartinContinanza <mcontinanza@controlunion.com>
2026-03-01 20:27:56 -0300

    Add files via upload

--- lib/supabase-client.ts
+++ lib/supabase-client.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://hvftwdsmwakqklnsymkd.supabase.co";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2ZnR3ZHNtd2FrcWtsbnN5bWtkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzOTc1NDAsImV4cCI6MjA4Nzk3MzU0MH0.PofSC-juYgEzw3oSwVRjCLvr9TTwQUZT5PK-VXjbfl4";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
