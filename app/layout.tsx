594f20b99343480f725efdef8c691dce3e986fff
MartinContinanza <mcontinanza@controlunion.com>
2026-03-07 20:34:23 -0300

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,15 @@
 "use client";
 import "./globals.css";
 import { AuthProvider } from "../lib/auth-context";
+import Script from "next/script";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
+      <head>
+        <Script src="https://cdn.botpress.cloud/webchat/v3.6/inject.js" strategy="afterInteractive" />
+        <Script src="https://files.bpcontent.cloud/2026/01/27/12/20260127124838-FGY84UIZ.js" strategy="afterInteractive" />
+      </head>
       <body>
         <AuthProvider>
           {children}
